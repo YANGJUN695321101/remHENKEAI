9a9220e6649056d3802bf0dc48c8510eaa8d3880
Leah White <129666645+YANGJUN695321101@users.noreply.github.com>
2023-05-01 21:46:16 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import QApplication
+from window import ChatWindow
+import sys
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    chat_window = ChatWindow()
+    chat_window.show()
+    sys.exit(app.exec_())
