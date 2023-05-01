d0e51997565c7ce6ed19b7b31cab8c5da9c1aeb6
Leah White <129666645+YANGJUN695321101@users.noreply.github.com>
2023-05-02 00:45:34 +0800

    Delete new_contact_button.py

--- new_contact_button.py
+++
@@ -1,6 +0,0 @@
-from PyQt5.QtWidgets import QPushButton
-
-class NewContactButton(QPushButton):
-    def __init__(self, parent=None):
-        super().__init__("新建联系人", parent)
-        # 新建联系人按钮部件，用于添加新联系人
